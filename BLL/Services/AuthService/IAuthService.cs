abe683baa3a96f387ea8bebf3b0aa026b4b31fd2
Youssef Montasr <Youssef.montasr@tildetech.ae>
2026-02-25 04:01:09 +0200

    add Auth implementation

---
+++ BLL/Services/AuthService/IAuthService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BLL.Dtos;
+
+namespace BLL.Services.AuthService
+{
+    public interface IAuthService
+    {
+        Task<AuthModel> RegisterAsync(RegisterRQ model);
+        Task<AuthModel>LoginAsync(LoginRQ model);
+    }
+}
